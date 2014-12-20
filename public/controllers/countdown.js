75f6f75f2fbeb8e4735d265e70d3f6519fd08e13
mishyTheBear <hamish.dickson@gmail.com>
2014-12-20 09:44:47 +0000

    add timer logic

--- public/controllers/countdown.js
+++ public/controllers/countdown.js
@@ -5,7 +5,30 @@
 
         return{
             restrict: 'E',
-            templateUrl: 'public/views/countdown.html'
+            templateUrl: 'public/views/countdown.html',
+            controller: [ '$timeout', '$scope', function($timeout, $scope) {
+                //this.initial = 1500000; // 25 min
+                this.initial = 2000; // 25 min
+                this.time = this.initial;
+
+                this.restartCountdown = function() {
+                    this.time = this.initial;
+                    this.startCountdown(30000);
+                };
+
+                this.startCountdown = function(inTime) {
+              
+                    if (inTime >= 1000) {
+                        this.time = inTime;
+                        $timeout(function() {
+                                this.startCountdown(inTime - 1000)
+                        }, 1000);
+                    }
+                    return;
+                };
+
+            }],
+            controllerAs: "countdown"
         };
     });
 })();
