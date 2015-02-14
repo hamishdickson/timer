f00c0b0ce3aaa7b3e2154835664315eaf1461e71
mishyTheBear <hamish.dickson@gmail.com>
2015-02-14 10:25:17 +0000

    minor refactoring after a lint run

--- public/controllers/countdown.js
+++ public/controllers/countdown.js
@@ -30,12 +30,12 @@
                         }, 1000);
                     }
                     return;
-                };
+                }
 
                 function msToTime(duration) {
-                    var seconds = parseInt((duration/1000)%60)
-                    , minutes = parseInt((duration/(1000*60))%60)
-                    , hours = parseInt((duration/(1000*60*60))%24);
+                    var seconds = parseInt((duration/1000)%60),
+                    minutes = parseInt((duration/(1000*60))%60),
+                    hours = parseInt((duration/(1000*60*60))%24);
 
                     hours = (hours < 10) ? "0" + hours : hours;
                     minutes = (minutes < 10) ? "0" + minutes : minutes;
